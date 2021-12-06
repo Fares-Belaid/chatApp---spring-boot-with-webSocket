45db5571f3f062ff829ed72f4f04a1ffe11f9607
Fares Belaid <fares.belaid@proxym-it.com>
2021-12-06 15:55:55 +0100

    chat App

---
+++ src/main/java/com/fares/chatApp/model/ChatMessage.java
@@ -0,0 +1,39 @@
+package com.fares.chatApp.model;
+
+public class ChatMessage {
+
+    private String content;
+
+    private String sender;
+
+    private MessageType type;
+
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public enum MessageType{
+        CHAT,LEAVE,JOIN
+    }
+}
